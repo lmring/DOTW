ca9fdf60d4ebe9d99510882a8aca57cec19f6483
Lauren Ring <lring@ucsd.edu>
2018-11-01 00:53:04 -0700

    final text, 5 hank text options, fixed spell catalog header & footer

--- js/introHCI.js
+++ js/introHCI.js
@@ -28,29 +28,55 @@ function StrengthChange() {
     var selectBox = document.getElementById("strength");
     var selected = selectBox.options[selectBox.selectedIndex].value;
 
-    if(selected < 8 && selected > 0){
-        $('#StrengthLowHank').show();
-				$('#StrengthLowHankImg').show();
-				$('#StrengthMediumHank').hide();
-				$('#StrengthMediumHankImg').hide();
-				$('#StrengthHighHank').hide();
-				$('#StrengthHighHankImg').hide();
+    if(selected < 5 && selected > 0){
+        $('#Strength1Hank').show();
+				$('#Strength1HankImg').show();
+				$('#Strength2Hank').hide();
+				$('#Strength3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Strength4Hank').hide();
+				$('#Strength5Hank').hide();
+				$('#Strength5HankImg').hide();
     }
-    else if(selected < 14 && selected > 7){
-        $('#StrengthLowHank').hide();
-				$('#StrengthLowHankImg').hide();
-				$('#StrengthMediumHank').show();
-				$('#StrengthMediumHankImg').show();
-				$('#StrengthHighHank').hide();
-				$('#StrengthHighHankImg').hide();
+		else if(selected < 9 && selected > 4){
+				$('#Strength1Hank').hide();
+				$('#Strength1HankImg').show();
+				$('#Strength2Hank').show();
+				$('#Strength3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Strength4Hank').hide();
+				$('#Strength5Hank').hide();
+				$('#Strength5HankImg').hide();
     }
-		else if(selected < 21 && selected > 13){
-  		$('#StrengthLowHank').hide();
-			$('#StrengthLowHankImg').hide();
-			$('#StrengthMediumHank').hide();
-			$('#StrengthMediumHankImg').hide();
-			$('#StrengthHighHank').show();
-			$('#StrengthHighHankImg').show();
+		else if(selected < 13 && selected > 8){
+				$('#Strength1Hank').hide();
+				$('#Strength1HankImg').hide();
+				$('#Strength2Hank').hide();
+				$('#Strength3Hank').show();
+				$('#Strength3HankImg').show();
+				$('#Strength4Hank').hide();
+				$('#Strength5Hank').hide();
+				$('#Strength5HankImg').hide();
+    }
+		else if(selected < 17 && selected > 12){
+				$('#Strength1Hank').hide();
+				$('#Strength1HankImg').hide();
+				$('#Strength2Hank').hide();
+				$('#Strength3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Strength4Hank').show();
+				$('#Strength5Hank').hide();
+				$('#Strength5HankImg').show();
+		}
+		else if(selected < 21 && selected > 16){
+				$('#Strength1Hank').hide();
+				$('#Strength1HankImg').hide();
+				$('#Strength2Hank').hide();
+				$('#Strength3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Strength4Hank').hide();
+				$('#Strength5Hank').show();
+				$('#Strength5HankImg').show();
 		}
 }
 
@@ -58,29 +84,55 @@ function DexterityChange() {
     var selectBox = document.getElementById("dexterity");
     var selected = selectBox.options[selectBox.selectedIndex].value;
 
-    if(selected < 8 && selected > 0){
-        $('#DexterityLowHank').show();
-				$('#StrengthLowHankImg').show();
-				$('#DexterityMediumHank').hide();
-				$('#StrengthMediumHankImg').hide();
-				$('#DexterityHighHank').hide();
-				$('#StrengthHighHankImg').hide();
-    }
-    else if(selected < 14 && selected > 7){
-        $('#DexterityLowHank').hide();
-				$('#StrengthLowHankImg').hide();
-				$('#DexterityMediumHank').show();
-				$('#StrengthMediumHankImg').show();
-				$('#DexterityHighHank').hide();
-				$('#StrengthHighHankImg').hide();
-    }
-		else if(selected < 21 && selected > 13){
-  		$('#DexterityLowHank').hide();
-			$('#StrengthLowHankImg').hide();
-			$('#DexterityMediumHank').hide();
-			$('#StrengthMediumHankImg').hide();
-			$('#DexterityHighHank').show();
-			$('#StrengthHighHankImg').show();
+		if(selected < 5 && selected > 0){
+				$('#Dexterity1Hank').show();
+				$('#Strength1HankImg').show();
+				$('#Dexterity2Hank').hide();
+				$('#Dexterity3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Dexterity4Hank').hide();
+				$('#Dexterity5Hank').hide();
+				$('#Strength5HankImg').hide();
+		}
+		else if(selected < 9 && selected > 4){
+				$('#Dexterity1Hank').hide();
+				$('#Strength1HankImg').show();
+				$('#Dexterity2Hank').show();
+				$('#Dexterity3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Dexterity4Hank').hide();
+				$('#Dexterity5Hank').hide();
+				$('#Strength5HankImg').hide();
+		}
+		else if(selected < 13 && selected > 8){
+				$('#Dexterity1Hank').hide();
+				$('#Strength1HankImg').hide();
+				$('#Dexterity2Hank').hide();
+				$('#Dexterity3Hank').show();
+				$('#Strength3HankImg').show();
+				$('#Dexterity4Hank').hide();
+				$('#Dexterity5Hank').hide();
+				$('#Strength5HankImg').hide();
+		}
+		else if(selected < 17 && selected > 12){
+				$('#Dexterity1Hank').hide();
+				$('#Strength1HankImg').hide();
+				$('#Dexterity2Hank').hide();
+				$('#Dexterity3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Dexterity4Hank').show();
+				$('#Dexterity5Hank').hide();
+				$('#Strength5HankImg').show();
+		}
+		else if(selected < 21 && selected > 16){
+				$('#Dexterity1Hank').hide();
+				$('#Strength1HankImg').hide();
+				$('#Dexterity2Hank').hide();
+				$('#Dexterity3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Dexterity4Hank').hide();
+				$('#Dexterity5Hank').show();
+				$('#Strength5HankImg').show();
 		}
 }
 
@@ -88,29 +140,55 @@ function ConstitutionChange() {
     var selectBox = document.getElementById("constitution");
     var selected = selectBox.options[selectBox.selectedIndex].value;
 
-    if(selected < 8 && selected > 0){
-        $('#ConstitutionLowHank').show();
-				$('#StrengthLowHankImg').show();
-				$('#ConstitutionMediumHank').hide();
-				$('#StrengthMediumHankImg').hide();
-				$('#ConstitutionHighHank').hide();
-				$('#StrengthHighHankImg').hide();
-    }
-    else if(selected < 14 && selected > 7){
-        $('#ConstitutionLowHank').hide();
-				$('#StrengthLowHankImg').hide();
-				$('#ConstitutionMediumHank').show();
-				$('#StrengthMediumHankImg').show();
-				$('#ConstitutionHighHank').hide();
-				$('#StrengthHighHankImg').hide();
-    }
-		else if(selected < 21 && selected > 13){
-  		$('#ConstitutionLowHank').hide();
-			$('#StrengthLowHankImg').hide();
-			$('#ConstitutionMediumHank').hide();
-			$('#StrengthMediumHankImg').hide()
-			$('#ConstitutionHighHank').show();
-			$('#StrengthHighHankImg').show();
+		if(selected < 5 && selected > 0){
+				$('#Constitution1Hank').show();
+				$('#Strength1HankImg').show();
+				$('#Constitution2Hank').hide();
+				$('#Constitution3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Constitution4Hank').hide();
+				$('#Constitution5Hank').hide();
+				$('#Strength5HankImg').hide();
+		}
+		else if(selected < 9 && selected > 4){
+				$('#Constitution1Hank').hide();
+				$('#Strength1HankImg').show();
+				$('#Constitution2Hank').show();
+				$('#Constitution3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Constitution4Hank').hide();
+				$('#Constitution5Hank').hide();
+				$('#Strength5HankImg').hide();
+		}
+		else if(selected < 13 && selected > 8){
+				$('#Constitution1Hank').hide();
+				$('#Strength1HankImg').hide();
+				$('#Constitution2Hank').hide();
+				$('#Constitution3Hank').show();
+				$('#Strength3HankImg').show();
+				$('#Constitution4Hank').hide();
+				$('#Constitution5Hank').hide();
+				$('#Strength5HankImg').hide();
+		}
+		else if(selected < 17 && selected > 12){
+				$('#Constitution1Hank').hide();
+				$('#Strength1HankImg').hide();
+				$('#Constitution2Hank').hide();
+				$('#Constitution3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Constitution4Hank').show();
+				$('#Constitution5Hank').hide();
+				$('#Strength5HankImg').show();
+		}
+		else if(selected < 21 && selected > 16){
+				$('#Constitution1Hank').hide();
+				$('#Strength1HankImg').hide();
+				$('#Constitution2Hank').hide();
+				$('#Constitution3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Constitution4Hank').hide();
+				$('#Constitution5Hank').show();
+				$('#Strength5HankImg').show();
 		}
 }
 
@@ -118,29 +196,55 @@ function IntelligenceChange() {
     var selectBox = document.getElementById("intelligence");
     var selected = selectBox.options[selectBox.selectedIndex].value;
 
-    if(selected < 8 && selected > 0){
-        $('#IntelligenceLowHank').show();
-				$('#StrengthLowHankImg').show();
-				$('#IntelligenceMediumHank').hide();
-				$('#StrengthMediumHankImg').hide();
-				$('#IntelligenceHighHank').hide();
-				$('#StrengthHighHankImg').hide();
-    }
-    else if(selected < 14 && selected > 7){
-        $('#IntelligenceLowHank').hide();
-				$('#StrengthLowHankImg').hide();
-				$('#IntelligenceMediumHank').show();
-				$('#StrengthMediumHankImg').show();
-				$('#IntelligenceHighHank').hide();
-				$('#StrengthHighHankImg').hide();
-    }
-		else if(selected < 21 && selected > 13){
-  		$('#IntelligenceLowHank').hide();
-			$('#StrengthLowHankImg').hide();
-			$('#IntelligenceMediumHank').hide();
-			$('#StrengthMediumHankImg').hide();
-			$('#IntelligenceHighHank').show();
-			$('#StrengthHighHankImg').show();
+		if(selected < 5 && selected > 0){
+				$('#Intelligence1Hank').show();
+				$('#Strength1HankImg').show();
+				$('#Intelligence2Hank').hide();
+				$('#Intelligence3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Intelligence4Hank').hide();
+				$('#Intelligence5Hank').hide();
+				$('#Strength5HankImg').hide();
+		}
+		else if(selected < 9 && selected > 4){
+				$('#Intelligence1Hank').hide();
+				$('#Strength1HankImg').show();
+				$('#Intelligence2Hank').show();
+				$('#Intelligence3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Intelligence4Hank').hide();
+				$('#Intelligence5Hank').hide();
+				$('#Strength5HankImg').hide();
+		}
+		else if(selected < 13 && selected > 8){
+				$('#Intelligence1Hank').hide();
+				$('#Strength1HankImg').hide();
+				$('#Intelligence2Hank').hide();
+				$('#Intelligence3Hank').show();
+				$('#Strength3HankImg').show();
+				$('#Intelligence4Hank').hide();
+				$('#Intelligence5Hank').hide();
+				$('#Strength5HankImg').hide();
+		}
+		else if(selected < 17 && selected > 12){
+				$('#Intelligence1Hank').hide();
+				$('#Strength1HankImg').hide();
+				$('#Intelligence2Hank').hide();
+				$('#Intelligence3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Intelligence4Hank').show();
+				$('#Intelligence5Hank').hide();
+				$('#Strength5HankImg').show();
+		}
+		else if(selected < 21 && selected > 16){
+				$('#Intelligence1Hank').hide();
+				$('#Strength1HankImg').hide();
+				$('#Intelligence2Hank').hide();
+				$('#Intelligence3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Intelligence4Hank').hide();
+				$('#Intelligence5Hank').show();
+				$('#Strength5HankImg').show();
 		}
 }
 
@@ -148,29 +252,55 @@ function WisdomChange() {
     var selectBox = document.getElementById("wisdom");
     var selected = selectBox.options[selectBox.selectedIndex].value;
 
-    if(selected < 8 && selected > 0){
-        $('#WisdomLowHank').show();
-				$('#StrengthLowHankImg').show();
-				$('#WisdomMediumHank').hide();
-				$('#StrengthMediumHankImg').hide();
-				$('#WisdomHighHank').hide();
-				$('#StrengthHighHankImg').hide();
-    }
-    else if(selected < 14 && selected > 7){
-        $('#WisdomLowHank').hide();
-				$('#StrengthLowHankImg').hide();
-				$('#WisdomMediumHank').show();
-				$('#StrengthMediumHankImg').show();
-				$('#WisdomHighHank').hide();
-				$('#StrengthHighHankImg').hide();
-    }
-		else if(selected < 21 && selected > 13){
-  		$('#WisdomLowHank').hide();
-			$('#StrengthLowHankImg').hide();
-			$('#WisdomMediumHank').hide();
-			$('#StrengthMediumHankImg').hide();
-			$('#WisdomHighHank').show();
-			$('#StrengthHighHankImg').show();
+		if(selected < 5 && selected > 0){
+				$('#Wisdom1Hank').show();
+				$('#Strength1HankImg').show();
+				$('#Wisdom2Hank').hide();
+				$('#Wisdom3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Wisdom4Hank').hide();
+				$('#Wisdom5Hank').hide();
+				$('#Strength5HankImg').hide();
+		}
+		else if(selected < 9 && selected > 4){
+				$('#Wisdom1Hank').hide();
+				$('#Strength1HankImg').show();
+				$('#Wisdom2Hank').show();
+				$('#Wisdom3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Wisdom4Hank').hide();
+				$('#Wisdom5Hank').hide();
+				$('#Strength5HankImg').hide();
+		}
+		else if(selected < 13 && selected > 8){
+				$('#Wisdom1Hank').hide();
+				$('#Strength1HankImg').hide();
+				$('#Wisdom2Hank').hide();
+				$('#Wisdom3Hank').show();
+				$('#Strength3HankImg').show();
+				$('#Wisdom4Hank').hide();
+				$('#Wisdom5Hank').hide();
+				$('#Strength5HankImg').hide();
+		}
+		else if(selected < 17 && selected > 12){
+				$('#Wisdom1Hank').hide();
+				$('#Strength1HankImg').hide();
+				$('#Wisdom2Hank').hide();
+				$('#Wisdom3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Wisdom4Hank').show();
+				$('#Wisdom5Hank').hide();
+				$('#Strength5HankImg').show();
+		}
+		else if(selected < 21 && selected > 16){
+				$('#Wisdom1Hank').hide();
+				$('#Strength1HankImg').hide();
+				$('#Wisdom2Hank').hide();
+				$('#Wisdom3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Wisdom4Hank').hide();
+				$('#Wisdom5Hank').show();
+				$('#Strength5HankImg').show();
 		}
 }
 
@@ -178,28 +308,54 @@ function CharismaChange() {
     var selectBox = document.getElementById("charisma");
     var selected = selectBox.options[selectBox.selectedIndex].value;
 
-    if(selected < 8 && selected > 0){
-        $('#CharismaLowHank').show();
-				$('#StrengthLowHankImg').show();
-				$('#CharismaMediumHank').hide();
-				$('#StrengthMediumHankImg').hide();
-				$('#CharismaHighHank').hide();
-				$('#StrengthHighHankImg').hide();
-    }
-    else if(selected < 14 && selected > 7){
-        $('#CharismaLowHank').hide();
-				$('#StrengthLowHankImg').hide();
-				$('#CharismaMediumHank').show();
-				$('#StrengthMediumHankImg').show();
-				$('#CharismaHighHank').hide();
-				$('#StrengthHighHankImg').hide();
-    }
-		else if(selected < 21 && selected > 13){
-  		$('#CharismaLowHank').hide();
-			$('#StrengthLowHankImg').hide();
-			$('#CharismaMediumHank').hide();
-			$('#StrengthMediumHankImg').show();
-			$('#CharismaHighHank').show();
-			$('#StrengthHighHankImg').show();
+		if(selected < 5 && selected > 0){
+				$('#Charisma1Hank').show();
+				$('#Strength1HankImg').show();
+				$('#Charisma2Hank').hide();
+				$('#Charisma3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Charisma4Hank').hide();
+				$('#Charisma5Hank').hide();
+				$('#Strength5HankImg').hide();
+		}
+		else if(selected < 9 && selected > 4){
+				$('#Charisma1Hank').hide();
+				$('#Strength1HankImg').show();
+				$('#Charisma2Hank').show();
+				$('#Charisma3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Charisma4Hank').hide();
+				$('#Charisma5Hank').hide();
+				$('#Strength5HankImg').hide();
+		}
+		else if(selected < 13 && selected > 8){
+				$('#Charisma1Hank').hide();
+				$('#Strength1HankImg').hide();
+				$('#Charisma2Hank').hide();
+				$('#Charisma3Hank').show();
+				$('#Strength3HankImg').show();
+				$('#Charisma4Hank').hide();
+				$('#Charisma5Hank').hide();
+				$('#Strength5HankImg').hide();
+		}
+		else if(selected < 17 && selected > 12){
+				$('#Charisma1Hank').hide();
+				$('#Strength1HankImg').hide();
+				$('#Charisma2Hank').hide();
+				$('#Charisma3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Charisma4Hank').show();
+				$('#Charisma5Hank').hide();
+				$('#Strength5HankImg').show();
+		}
+		else if(selected < 21 && selected > 16){
+				$('#Charisma1Hank').hide();
+				$('#Strength1HankImg').hide();
+				$('#Charisma2Hank').hide();
+				$('#Charisma3Hank').hide();
+				$('#Strength3HankImg').hide();
+				$('#Charisma4Hank').hide();
+				$('#Charisma5Hank').show();
+				$('#Strength5HankImg').show();
 		}
 }
