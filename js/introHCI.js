47963998f73668877fc45b748c86c1a3d6b3177e
Jodie Lim <jal260@ucsd.edu>
2018-11-04 00:26:28 -0700

    DID MY TOGGLEgit pull

--- js/introHCI.js
+++ js/introHCI.js
@@ -483,10 +483,17 @@ function checkArmorName(armor) {
 
 
 function toggleContent() {
-    var x = document.getElementById("toggleThis");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+	var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
 }
