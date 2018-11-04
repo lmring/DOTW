dc8165f51a4d1afc3f419a61c8fcde726310e8d3
Jodie Lim <jal260@ucsd.edu>
2018-11-04 01:18:38 -0700

    Finished Armor sans Lily. Added Read Mes and Full Text Uploaded.

--- js/introHCI.js
+++ js/introHCI.js
@@ -515,19 +515,3 @@ function checkArmorType(armor) {
 	return armor.type.includes(type);
 }
 //armor end ************************************************************
-
-function toggleContent() {
-	var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
-}
