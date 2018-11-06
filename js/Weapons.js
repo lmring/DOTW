1ce4b4f63b202c5cfe79c465f188bb45dde4098b
Lauren Ring <lring@ucsd.edu>
2018-11-05 19:28:34 -0800

    lilly code skeleton

--- js/Weapons.js
+++ js/Weapons.js
@@ -1,5 +1,13 @@
-function lilyChange(){
+function lillyChange(){
   var selectBox = document.getElementById("weapons");
   var selected = selectBox.options[selectBox.selectedIndex].value;
-  
+
+  if(selected=="club"){
+    $('.weapon').hide();
+    $('#LilllyClub').show();
+  }
+  if(selected=="dagger"){
+    $('.weapon').hide();
+    $('#LilllyDagger').show();
+  }
 }
